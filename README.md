64abb98c630214c2892baca363aa6c2f40086cd1
Krzysztof Maicher <krzysztof.maicher@gmail.com>
2018-02-28 16:00:35 +0100

    Add support for sass

--- README.md
+++ README.md
@@ -5,11 +5,19 @@
     $ npm i webpack-cli -D
     $ npm i webpack-dev-server@2 --save-dev
     
+## Step 2
+
+    $ npm install css-loader node-sass sass-loader extract-text-webpack-plugin
+    
 ## How to run on dev
 
-    $ webpack-dev-server webpack.config.js
+    $ webpack-dev-server
     
 ## How to compile
 
     $ npx webpack --config webpack.config.js
  
+## On any problems first try
+
+    $ rm -rf node_modules/
+    $ npm i
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = [{
   entry: './apps/main/assets/index.js',
   output: {
     filename: './public/assets/index.js',
   }
+},
+{
+  entry: './apps/main/assets/index.sass',
+  output: {
+    filename: './public/assets/index.css',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("./public/assets/index.css", {
+      allChunks: true
+    })
+  ]
 }
 ];
